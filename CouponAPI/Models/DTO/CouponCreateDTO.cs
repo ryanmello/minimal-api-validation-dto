419aceea54afcaacf6f84d9698fea45066459a56
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-04-06 08:55:49 -0700

    coupon object dtos

---
+++ CouponAPI/Models/DTO/CouponCreateDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace CouponAPI.Models.DTO
+{
+	public class CouponCreateDTO
+	{
+		public string Name { get; set; } = string.Empty;
+		public int Percent { get; set; }
+		public bool IsActive { get; set; }
+	}
+}
--- CouponAPI/Program.cs
+++ CouponAPI/Program.cs
@@ -1,5 +1,6 @@
 using CouponAPI.Data;
 using CouponAPI.Models;
+using CouponAPI.Models.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.OpenApi;
 
@@ -19,34 +20,77 @@ if (app.Environment.IsDevelopment())
 	app.UseSwaggerUI();
 }
 
-app.MapGet("api/coupon", () => {
+/* app.MapGet("api/coupon, () => { } returns a list of coupon objects
+ * with a success status code of 200 upon creation of a coupon */
+
+app.MapGet("api/coupon", (ILogger<Program> _logger) => {
+	_logger.Log(LogLevel.Information, "Getting all coupons");
 	return Results.Ok(CouponStore.Coupons);
 }).Produces<IEnumerable<Coupon>>(200);
+
+/* app.MapGet("api/coupon/{id:int}, () => { } returns a singele coupon.
+ * the method requires a paramter id and will return the coupon object
+ * at the give id address */
 
 app.MapGet("api/coupon/{id:int}", (int id) => {
 	return Results.Ok(CouponStore.Coupons.FirstOrDefault(c => c.Id == id));
 }).Produces<Coupon>(200);
 
+/* app.MapPost("api/coupon, () => { } creates a singele coupon object.
+ * the method requires a coupon body. the method will validate the input.
+ * inputs of an object with an id not equal to 0, with an empty name,
+ * or a coupon that already exists, will throw a 400 error. all other 
+ * sucessful inputs will create a coupon object and append it to the 
+ * list of coupons */
 
-app.MapPost("api/coupon", ([FromBody] Coupon coupon) => {
-	if (coupon.Id != 0 || string.IsNullOrEmpty(coupon.Name))
+app.MapPost("api/coupon", ([FromBody] CouponCreateDTO coupon_C_DTO) => {
+	/* checks if the name on the object being passed in null and if the coupon already exits */
+	if (string.IsNullOrEmpty(coupon_C_DTO.Name))
 	{
 		return Results.BadRequest("Invalid Id or Coupon Name");
 	}
-
-	if (CouponStore.Coupons.FirstOrDefault(c => c.Name.ToLower() == coupon.Name.ToLower()) != null)
+	if (CouponStore.Coupons.FirstOrDefault(c => c.Name.ToLower() == coupon_C_DTO.Name.ToLower()) != null)
 	{
 		return Results.BadRequest("Coupon already exists.");
 	}
 
+	/* creates a new coupon object with the given DTO information */
+	Coupon coupon = new()
+	{
+		IsActive = coupon_C_DTO.IsActive,
+		Name = coupon_C_DTO.Name,
+		Percent = coupon_C_DTO.Percent
+	};
+
+	/* sets the current coupons id to one greater than the ID of the previous coupon */
 	coupon.Id = CouponStore.Coupons.OrderByDescending(c => c.Id).FirstOrDefault().Id + 1;
 	CouponStore.Coupons.Add(coupon);
-	return Results.Ok(coupon);
-}).Produces<Coupon>(201).Produces(400);
+	Coupon couponDTO = new()
+	{
+		Id = coupon.Id,
+		IsActive = coupon.IsActive,
+		Name = coupon.Name,
+		Percent = coupon.Percent,
+		Created = coupon.Created
+	};
+	return Results.Ok(couponDTO);
+}).WithName("CreateCoupon").Accepts<CouponCreateDTO>("application/json").Produces<CouponDTO>(201).Produces(400);
+
+/*
+ * 
+ * 
+ * 
+ */
 
 app.MapPut("api/coupon", () => {
 
 });
+
+/*
+ * 
+ * 
+ * 
+ */
 
 app.MapDelete("api/coupon/{id:int}", (int id) => {
 
